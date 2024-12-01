e603622d49c327740fc6ab9b6771479e1cf0b2d0
soma <somofgit@gmail.com>
2024-12-01 15:38:32 +0900

    準備

--- extension.js
+++ extension.js
@@ -1,33 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+/*
+目標
+  コード読む時に使う一時的なメモ欄が、PrimarySidebarに欲しい。
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+TODO
+  o PrimarySidebarに項目を追加
+	x PrimarySidebarにテキストを表示する
+	x PrimarySidebarに入力欄を追加
+	x アイコン差し替え
+	x TypeScriptに挑戦
+*/
+
+const vscode = require('vscode');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sidenote" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('sidenote.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello VSCode Extensions!');
 	});
-
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
